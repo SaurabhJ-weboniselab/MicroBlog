f414c8d9537a872ddfcf3e19d24db41d40bdf3e6
SaurabhJ-webonise <saurabh.j@weboniselab.com>
2023-03-06 17:57:48 +0530

    feature-crud | dry concept used

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
   def index
     @post = Post.all
   end
 
-  def show
-    @post = set_post
-  end
+  def show; end
 
   def new
     @post = Post.new
   end
 
-  def edit
-    @post = set_post
-  end
+  def edit; end
 
   def create
     @post = Post.new(post_params)
@@ -29,15 +26,12 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = set_post
     @post.destroy
 
     redirect_to root_path, status: :see_other
   end
 
   def update
-    @post = set_post
-
     if @post.update(post_params)
       redirect_to @post
     else
