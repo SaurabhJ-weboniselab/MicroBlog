ce410a6a18edb2b699dbb68a8b23a163438e8f5d
SaurabhJ-webonise <saurabh.j@weboniselab.com>
2023-03-13 19:39:59 +0530

    feature-database-orm | comments and author added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_173003) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_133202) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_authors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -38,8 +51,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_173003) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.boolean "is_published", default: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "authors"
 end
