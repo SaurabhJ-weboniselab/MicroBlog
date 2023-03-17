88084b4e849fe0cea3d98a75afc372d4e19b07fb
SaurabhJ-webonise <saurabh.j@weboniselab.com>
2023-03-17 12:10:30 +0530

    feature-tdd | post spec and author spec tdd assignment

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'method checking' do
+    it 'if data stores using new' do
+      post = Post.new({ title: 'mayur', body: 'good boy vaaf abvf av' }).save
+      expect(post).to be true
+    end
+
+    it 'if data stores using create' do
+      post = Post.create({ title: 'mayur', body: 'good boy vaaf abvf av' }).save
+      expect(post).to be true
+    end
+
+    it 'if first data deletes' do
+      post = Post.first.delete
+      expect(post).to be true
+    end
+  end
 end
